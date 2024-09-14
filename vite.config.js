8e05c4009e2ad5b89fa5c52cf4dbcff11f6feb4c
Ram kusharam <ramkushram120@gmail.com>
2024-09-15 01:04:13 +0530

    Update Vite configuration to set build directory to 'dist'

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // Ensure this matches your Render settings
+  },
   plugins: [react()],
 })
